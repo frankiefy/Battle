3f20c607b0a48b33247b98c148b89fbc7494236e
frankie <franklin.shaw@gmail.com>
2017-01-18 12:20:17 +0000

    end step 20

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -6,6 +6,16 @@ feature 'Enter names' do
     fill_in :player_1_name, with: 'Frank'
     fill_in :player_2_name, with: 'Alf'
     click_button 'Submit'
-    expect(page).to have_content 'Frank vs. Alf'
+    expect(page).to have_content 'FRANK vs. ALF'
+  end
+end
+
+feature 'Enemy hit points' do
+  scenario 'at start, see enemy hit points' do
+    visit('/')
+    fill_in :player_1_name, with: 'Frank'
+    fill_in :player_2_name, with: 'Alf'
+    click_button 'Submit'
+    expect(page).to have_content 'Alf: 60/60 HP'
   end
 end
